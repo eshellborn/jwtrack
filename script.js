e86262a109e37a7ad43a48e7f370aa7307c43b05
eshellborn <eshellborn@gmail.com>
2026-08-07 10:29:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -251,7 +251,7 @@ async function syncProgressChange(itemId, done) {
 }
 
 async function loadCloudProgress() {
-  console.trace("loadCloudProgress");
+  console.log("hello");
   if (!supabaseClient || !state.user) return;
 
   setAccountStatus("Syncing progress...");
